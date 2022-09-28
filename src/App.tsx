58d1bb52e49eaaef1bd33d98d495b01d6bf660fb
Insu Hong <hinsu93@gmail.com>
2022-09-28 10:52:50 +0900

    Setup Coin Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import Router from "./Router";
+
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import ReactDOM from "react-dom/client";
-import App from './App';
-
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
-
